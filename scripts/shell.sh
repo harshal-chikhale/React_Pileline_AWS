70b3fb6480af5bcceeed7096d38e21105ac07848
Harshal Chikhale <harshalchikhale9822@gmail.com>
2023-02-09 23:47:01 +0530

    first commit

---
+++ scripts/shell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Set ownership for all folders and files
+
+chmod 777 /home/ec2-user/server/scripts/after_install.sh
+
+chmod 777 /home/ec2-user/server/scripts/app_start.sh
+
+chmod 777 /home/ec2-user/server/scripts/before_install.sh
---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+export default function App() {
+  return (
+    <div><h1> AWS React Application</h1>    </div>  )
+}
